c98dab938916399e80a6b824917d1a785a922d1f
Rusiate Sevutaka <rkoroi15@gmail.com>
2023-01-19 19:07:47 +1100

    Gliphys ajax call

---
+++ utils/ajax_connect.php
@@ -0,0 +1,22 @@
+<?php 	
+
+require("service.php");	
+if (isset($_REQUEST['action']) && !empty($_REQUEST['action']) )
+{
+	$action = htmlentities($_REQUEST['action']);  
+	$version_of_app = 1;  
+	switch ($action)
+	{			
+		case 'read_glipyhs_by_search_item':
+			$search_item 	= $_REQUEST['value'];
+			$all_gliphys 	= create_search_api($search_item);
+			echo json_encode(array("output" => $all_gliphys));
+		break;
+		default:
+			$no_action = '<h1>There was no action passed!!</h1>' ;
+	    	echo json_encode(array("output" => $no_action));
+		break;
+	}
+}
+
+?>
--- utils/service.php
+++ utils/service.php
@@ -2,7 +2,9 @@
 
 function create_search_api($text) {
 	//api_key - I7xDtk4NVj3ZHdzHIgo6183bQuPfwZPj
+	$items = "There is no gliphys found";
+	return $items;
 }
 
-<?>
+?>
 
