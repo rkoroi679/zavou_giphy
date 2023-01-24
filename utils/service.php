c5d882b9679eaaca961d02dfee17e17d3392046d
Rusiate Sevutaka <rkoroi15@gmail.com>
2023-01-24 19:12:49 +1100

    Fix get correct URL

--- utils/service.php
+++ utils/service.php
@@ -3,18 +3,15 @@
 function create_search_api($text) {
 	$api_key 	= 'I7xDtk4NVj3ZHdzHIgo6183bQuPfwZPj';
 	$url 		= "http://api.giphy.com/v1/gifs/search?q=".$text."&api_key=".$api_key."&limit=5";
-	$response 	= json_decode(file_get_contents($url));
-	// var_dump($response);
-	// exit;
-	$result  = [];
-
+	$response 	= json_decode(file_get_contents($url));	
+	$result  	= [];
 	foreach ($response->data as $key => $value) 
 	{
-		$url 			= $value->embed_url;
+		$url 			= $value->images->downsized->url;
 		$title 			= $value->title;
 		$temp_result 	= array($url, $title);
 		array_push($result, $temp_result);
-	}
+	}	
 	return $result;
 }
 
